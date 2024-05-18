0a2caaaa7bc3f301a9acd652561501718b5a71b5
nageshmali <malinagesh60@gmail.com>
2024-05-18 09:08:19 +0530

    picturify backup

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TranformationsPage = () => {
+    return (
+        <div>Transformations Page</div>
+    )
+}
+
+export default TranformationsPage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddTransformationType = () => {
+    return (
+        <div>Add Tranformation Type</div>
+    )
+}
+
+export default AddTransformationType
